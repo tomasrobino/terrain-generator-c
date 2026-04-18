d94f644c985b9ac8a583e37abc776c8e5db8b32e
Tomas Robino <tomaserobino@gmail.com>
2026-04-19 01:59:42 +0200

    empty diffusion_limited_aggregation ()

---
+++ generation.c
@@ -0,0 +1,12 @@
+//
+// Created by Tomas on 19/4/2026.
+//
+
+#include "generation.h"
+
+
+
+
+void diffusion_limited_aggregation(uint64_t width, uint64_t height, uint64_t matrix) {
+
+}
---
+++ generation.h
@@ -0,0 +1,18 @@
+//
+// Created by Tomas on 19/4/2026.
+//
+
+#ifndef TERRAIN_GENERATION_C_GENERATION_H
+#define TERRAIN_GENERATION_C_GENERATION_H
+
+#include <stdint.h>
+
+/**
+ *
+ * @param width Width of the matrix
+ * @param height Height of the matrix
+ * @param matrix Empty stack-allocated matrix to apply DLA to
+ */
+void diffusion_limited_aggregation(uint64_t width, uint64_t height, uint64_t matrix);
+
+#endif //TERRAIN_GENERATION_C_GENERATION_H
