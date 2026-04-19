389a87d309de0947fa509df1c1389f6b966bc47e
Tomas Robino <tomaserobino@gmail.com>
2026-04-19 23:22:07 +0200

    saving to pgm

--- main.c
+++ main.c
@@ -7,6 +7,6 @@
 int main(void) {
     uint8_t matrix[HEIGHT][WIDTH] = {0};
     diffusion_limited_aggregation(WIDTH, HEIGHT, matrix, 100);
-    binary_colors_matrix(WIDTH, HEIGHT, matrix)
+    binary_colors_matrix(WIDTH, HEIGHT, matrix);
     save_pgm("output.pgm", WIDTH, HEIGHT, matrix);
 }
