894dd424a4e99d6e0a2403345e1687177845c921
Tomas Robino <tomaserobino@gmail.com>
2026-04-19 23:24:18 +0200

    PIXEL_AMOUNT

--- main.c
+++ main.c
@@ -3,10 +3,11 @@
 
 #define HEIGHT 20
 #define WIDTH 20
+#define PIXEL_AMOUNT (HEIGHT*WIDTH*0.4)
 
 int main(void) {
     uint8_t matrix[HEIGHT][WIDTH] = {0};
-    diffusion_limited_aggregation(WIDTH, HEIGHT, matrix, 100);
+    diffusion_limited_aggregation(WIDTH, HEIGHT, matrix, PIXEL_AMOUNT);
     binary_colors_matrix(WIDTH, HEIGHT, matrix);
     save_pgm("output.pgm", WIDTH, HEIGHT, matrix);
 }
